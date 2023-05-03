367951d51dd01bb7104552e7c3a871b9de491cce
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-02 19:42:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blackberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Nard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blackberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Nard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
