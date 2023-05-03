6345413b8f3d3fe45cbd3dd2fe074b70fdad1502
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-02 19:35:06 -0600

    Update streamlit_app.py
    added:
    streamlit.header('Breakfast Menu')
    streamlit.text('Omega 3 & Blackberry Oatmeal')
    streamlit.text('Kale, Spinach & Rocket Smoothie')
    streamlit.text('Nard-Boiled Free-Range Egg')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Nard-Boiled Free-Range Egg')
+
