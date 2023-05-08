9fb3206d92208dd4cd8a41dd626cdcf8668a364e
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 12:56:51 -0600

    Update streamlit_app.py
    # New section to display fruityvice api response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+# New section to display fruityvice api response.
+streamlit.header('Fruityvice Fruit Advice')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
