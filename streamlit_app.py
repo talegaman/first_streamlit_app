508e899a216054411ab03f11e98be27129e2c41f
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 17:28:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-#streamlit.stop()
-
-
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -53,5 +49,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
+
 
 
