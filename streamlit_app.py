5000e4f9a4be1e908838598042170464543c36da
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 16:16:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -43,6 +43,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+
