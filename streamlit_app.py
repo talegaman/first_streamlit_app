85928023a0d1a3edad28b76df9f24cc99e635af7
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 12:53:51 -0600

    Update streamlit_app.py
    Get request from website link and display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -18,3 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# Get request from website link and display.
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
