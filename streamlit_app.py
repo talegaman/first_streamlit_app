86e589d2eaf9ee6d9af021a3c8e6866e19ee8fde
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 13:25:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # New section to display fruityvice api response.
 streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlite.write('The user entered', fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
