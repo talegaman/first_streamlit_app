878466fbe5548bcd0babaf60b57a29aeb8d5a4f3
Alejandro Pedrero Montiel <46499182+talegaman@users.noreply.github.com>
2023-05-08 18:10:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
-streamlit.header("The fruit load list contains:")
-# Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-         return my_cur.fetchall()
-
-# Add button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('The user entered', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blackberry Oatmeal')
@@ -50,16 +31,17 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
     
+
 streamlit.header('Fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
+       streamlit.error("Please select a fruit to get information.")
   else:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)    
 except URLError as e:
-  streamlit.error()
+ streamlit.error()
 
 
 
